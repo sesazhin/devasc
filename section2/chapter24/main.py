8fc144a52a040f31ff869ef2eb46e5c4d5e2eb12
sesazhin <sesazhin@cisco.com>
2020-12-23 02:24:34 +0300

    updated section 2.4 code (TDD)

--- section2/chapter24/main.py
+++ section2/chapter24/main.py
@@ -1,7 +1,14 @@
 import re
 import os
 
+
 class ConfigurationParser:
+    def __init__(self):
+        config_filename = 'config.txt'
+
+        with open(config_filename, 'r') as config_fp:
+            self.deviceConfig = config_fp.read()
+
     def parseCustomerNames(self):
         '''
         files = os.listdir(os.curdir)
@@ -9,10 +16,59 @@ class ConfigurationParser:
         '''
 
         # config_filename = os.path.join('section2', 'chapter24', 'config.txt')
-        config_filename = 'config.txt'
 
-        with open(config_filename, 'r') as config_fp:
-            deviceConfig = config_fp.read()
-            customerNamePattern = r'ip vrf ([a-zA-Z_]+)\n'
-            customerNames = re.findall(customerNamePattern, deviceConfig)
-            return customerNames
+        customerNamePattern = r'ip vrf ([a-zA-Z_]+)\n'
+        customerNames = re.findall(customerNamePattern, self.deviceConfig)
+        return customerNames
+
+    def parseCustomerVlan(self, customerName):
+        '''
+        files = os.listdir(os.curdir)
+        print(files)
+        '''
+
+        # config_filename = os.path.join('section2', 'chapter24', 'config.txt')
+
+        '''
+        interface_pattern = (
+            r"interface GigabitEthernet0\/0\.([0-9]+)\n"
+            rf" encapsulation dot1Q [0-9]+\n ip vrf forwarding {customerName}"
+        )
+        '''
+
+        interface_pattern = (
+            r"interface GigabitEthernet0\/0\.([0-9]+)\n"
+            r"  encapsulation dot1Q [0-9]+\n"
+            f"  ip vrf forwarding {customerName}"
+        )
+
+        print(interface_pattern)
+
+        allCustomerSubInterfaces = re.search(interface_pattern, self.deviceConfig)
+
+        # customerVlanPattern = r'encapsulation dot1Q ([0-9]+)\n'
+        # customerVlans = re.findall(customerVlanPattern, self.deviceConfig)
+        print(allCustomerSubInterfaces.group(0))
+        return int(allCustomerSubInterfaces.group(1))
+
+    def parseCustomerIp(self, customer_vlan):
+        ip_pattern = (
+            rf"GigabitEthernet0/0\.{customer_vlan}[ ]+([0-9\.]+)"
+        )
+        print(ip_pattern)
+        ip_addr = re.search(ip_pattern, self.deviceConfig)
+        print(ip_addr.group(0))
+        return ip_addr.group(1)
+
+    def parseCustomerData(self):
+        customerData = {}
+        customerNames = self.parseCustomerNames()
+        for customerName in customerNames:
+            vlan = self.parseCustomerVlan(customerName)
+            ip_address = self.parseCustomerIp(vlan)
+            customerData[customerName] = [vlan, ip_address]
+
+            print(customerData)
+
+        return customerData
+
--- section2/chapter24/py_unittest_parser.py
+++ section2/chapter24/py_unittest_parser.py
@@ -11,6 +11,32 @@ class TestParse(unittest.TestCase):
         self.assertEqual(list, type(parsed_names))
         self.assertEqual(expected_names, parsed_names)
 
+    def test_parse_customer_vlan(self):
+        cp = ConfigurationParser()
+        expected_vlans = {'CUSTOMER_A': 100, 'CUSTOMER_B': 101}
+        customer_name = 'CUSTOMER_A'
+        parsed_vlan = cp.parseCustomerVlan(customer_name)
+        print(parsed_vlan)
+        # self.assertEqual(int, type(parsed_vlan))
+        self.assertEqual(expected_vlans[customer_name], parsed_vlan)
+
+    def test_parse_customer_ip(self):
+        cp = ConfigurationParser()
+        expected_ips = {'100': '10.10.100.1', '101': '10.10.101.1'}
+        customer_vlan = '100'
+        parsed_ip = cp.parseCustomerIp(customer_vlan)
+        print(parsed_ip)
+        # self.assertEqual(int, type(parsed_vlan))
+        self.assertEqual(expected_ips[customer_vlan], parsed_ip)
+
+    def test_parse_customer_data(self):
+        cp = ConfigurationParser()
+        expected_data = {'CUSTOMER_A': [100, '10.10.100.1'],
+                         'CUSTOMER_B': [101, '10.10.101.1']}
+
+        parsed_data = cp.parseCustomerData()
+        self.assertEqual(expected_data, parsed_data)
+
 
 if __name__ == '__main__':
     unittest.main()
